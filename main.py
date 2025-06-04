254bdcd65df80855ce426473d7207010a034bab9
Justin Jones <jjones@costar.com>
2025-06-04 16:03:58 -0400

    added cors middleware

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from .database import engine, create_db_and_tables 
 from fastapi import FastAPI
+from starlette.middleware import Middleware
+from starlette.middleware.cors import CORSMiddleware
 from sqlmodel import create_engine, SQLModel
 from contextlib import asynccontextmanager
 from .routers import habits, trackers, users
@@ -13,7 +15,32 @@ async def lifespan(app: FastAPI):
     create_db_and_tables(engine)
     yield
 
-app = FastAPI(lifespan=lifespan)
+# middleware = [
+#     Middleware(
+#         CORSMiddleware,
+#         allow_origins=['*'],
+#         allow_credentials=True,
+#         allow_methods=['*'],
+#         allow_headers=['*']
+#     )
+# ]
+
+app = FastAPI(
+    lifespan=lifespan,
+    # middleware=middleware
+)
+
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(users.router)
 app.include_router(habits.router)
